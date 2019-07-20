1d4ee1c20efc35adbee95ae1a0be8ff31fd479f2
satyam kumar <satyamkumar5254@gmail.com>
2019-07-20 21:04:06 +0530

    query selector done

---
+++ index.js
@@ -0,0 +1,21 @@
+var tag = document.getElementById('highlighted')
+
+document.getElementById('hello').innerHTML  = 'Hello'; 
+
+var tags = document.getElementsByClassName('bolded')
+
+
+var tagname = document.getElementsByTagName('li')
+
+
+
+// query selector
+// query selector select first matching element
+// queryselector works like css selector as selector works in css as it is works here
+
+
+
+var h1 = document.querySelector("h1")
+
+var li = document.querySelector('li a.special') // we can use in this way
+
