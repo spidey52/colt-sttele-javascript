9fb03fbd9f0f336a34508c7fbbc7eca485dd31f7
satyam kumar <satyamkumar5254@gmail.com>
2019-07-20 11:01:45 +0530

    changing body color per second

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-var movie = [
-    {
-        title: "son of sardar",
-        rating: "3.4",
-        hasWatched: true
-    },
-    {
-        title: "life of pi",
-        rating: "4.5",
-        hasWatched: false
-    },
-    {
-        title: "super 30",
-        rating: "4.1",
-        hasWatched: true
-    }
-]
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
-for (let i=0; i<movie.length; i++){
-    if (movie[i].hasWatched){
-        console.log(`you have watched ${movie[i].title} - ${movie[i].rating} stars.`)
-    }else{
-        console.log(`you have not watched ${movie[i].title} - ${movie[i].rating} stars.`)
+console.log(body)
+
+body.style.background = "#252525";
+h1.style.color = "pink";
+
+var isBlack = true;
+setInterval( function()  {
+    if (body.style.background == "rgb(27, 27, 27)" ){
+        body.style.background = "red";
+    } else {
+        body.style.background = "rgb(27, 27, 27)" ;
+        
     }
-}
+    isBlack = !isBlack;
+}, 100);
